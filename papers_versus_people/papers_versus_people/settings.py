0ea0e353f724fbe51fa79ae0be19206603ab4562
Caio Fontes <caiofontesc@gmail.com>
2023-05-04 21:59:07 -0300

    chore: fix docker-compose

--- papers_versus_people/papers_versus_people/settings.py
+++ papers_versus_people/papers_versus_people/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+ROOT_DIR = BASE_DIR.parent
+BACKEND_DIR = BASE_DIR
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -80,7 +81,7 @@ WSGI_APPLICATION = "papers_versus_people.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BACKEND_DIR / "db.sqlite3",
     }
 }
 
